cd02c890af077ec419693c5d3e41ea039d482f97
venu564 <venu.07564@gmail.com>
2026-09-26 18:17:35 +0530

    Adding folders

--- newspaper/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('<int:page_num>/',views.page_num_view),
-    path('<str:topic>/',views.news_paper),
-    path('',views.welcome_view),
-]
-
